a7835e6bc3597e9705a9321ab9a7c867bf06457b
Dylan Plecki <dylanplecki@gmail.com>
2014-09-23 12:55:03 -0500

    Update user model

---
+++ src/com/weighin/api/models/email.java
@@ -0,0 +1,22 @@
+package com.weighin.api.models;
+
+import java.util.regex.*;
+
+/**
+ * Created by Dylan on 9/23/2014.
+ */
+public class email {
+
+    static boolean verify(String email) {
+        final Matcher matcher = email_regex.matcher(email);
+        return matcher.matches();
+    }
+
+    static final Pattern email_regex = Pattern.compile("" + // Start of the RFC 5322 Official RegEx Pattern
+            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-" +
+            "\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]" +
+            "*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
+            "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-" +
+            "\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+    );
+}
--- src/com/weighin/api/models/user.java
+++ src/com/weighin/api/models/user.java
@@ -3,38 +3,52 @@ package com.weighin.api.models;
 import java.util.*;
 import java.security.MessageDigest;
 
+import com.weighin.api.models.email.*;
+
 /**
  * Created by Dylan on 9/19/2014.
  */
 public class user {
 
-    private String username;
-    private String email;
-    private String password_hash;
-    private Map<String,String> attributes;
+    private String _username;
+    private String _email;
+    private String _password_hash;
+    private Map<String,String> _attributes;
 
-    public String getUsername() { return username; };
-    public String getEmail() { return username; };
-    public String getPasswordHash() { return username; };
-    public Map<String,String> getAttributes() { return attributes; };
-    public String getAttribute(String name) { return attributes.getOrDefault(name, null); };
+    public String getUsername() { return _username; }
+    public String getEmail() { return _email; }
+    public String getPasswordHash() { return _password_hash; }
+    public Map<String,String> getAttributes() { return _attributes; }
+    public String getAttribute(String name) { return _attributes.getOrDefault(name, null); }
 
-    public void setUsername(String username) { this.username = username; };
-    public void setEmail(String email) { this.email = email; };
-    public void setPasswordHash(String password_hash) { this.password_hash = password_hash; };
-    public void setAttributes(Map<String,String> attributes) { this.attributes = attributes; };
+    public void setUsername(String username) { this._username = username; }
+    public void setPassword(String password) throws Exception {
+        this._password_hash = hashPassword(password);
+    }
+    public void setPasswordHash(String password_hash) { this._password_hash = password_hash; }
+    public void setAttributes(Map<String,String> attributes) { this._attributes = attributes; }
+
+    public void setEmail(String email) {
+        try {
+            this._email = verifyEmail(email);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public boolean checkPassword(String password) throws Exception {
+        return this._password_hash == hashPassword(password);
+    }
 
     private String hashPassword(String password) throws Exception {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(password.getBytes("UTF-8"));
         return new String(md.digest(), "UTF-8");
-    };
+    }
 
-    public void setPassword(String password) throws Exception {
-        this.password_hash = hashPassword(password);
-    };
-
-    public boolean checkPassword(String password) throws Exception {
-        return this.password_hash == hashPassword(password);
-    };
+    private String verifyEmail(String email) throws Exception {
+        if (!com.weighin.api.models.email.verify(email)) throw new Exception("Email address format isn't valid");
+        // Do any email manipulation
+        return email;
+    }
 }
