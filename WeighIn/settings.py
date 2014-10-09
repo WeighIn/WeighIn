7738f2b3e2c37fc37fb30f5d2fc65d6123463451
Dylan Plecki <dylanplecki@gmail.com>
2014-10-08 20:44:14 -0500

    Add support for static files

--- WeighIn/settings.py
+++ WeighIn/settings.py
@@ -95,7 +95,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
