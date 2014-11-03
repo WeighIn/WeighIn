c6a8399cf2f96a604189191208520d80e11de36d
kazuuchikata <kazuuchikata@gmail.com>
2014-11-03 11:48:06 -0600

    index no longer broken, added logo with animation

--- static/frontsite/index.js
+++ static/frontsite/index.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
-	$('.navigation .fa').hover(
+	$('.navbar .fa').hover(
 		function() {
 			$(this).css("opacity", "0.7");
 		},function() {
 			$(this).css("opacity", "1.0");
 		});
+	$('.logo').hover(
+		function() {
+			$(this).stop().animate(
+			  {rotation: 360},
+			  {
+			    duration: 500,
+			    step: function(now, fx) {
+			      $(this).css({"transform": "rotate("+now+"deg)"});
+			    }
+			  }
+			);
+		},function() {
+			$(this).stop().animate(
+			  {rotation: -360},
+			  {
+			    duration: 500,
+			    step: function(now, fx) {
+			      $(this).css({"transform": "rotate("+now+"deg)"});
+			    }
+			  }
+			);
+		});
+
 });
 
