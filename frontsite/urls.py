b3050cf778bc65753d0e39a602d4904c27605cb6
Dylan Plecki <dylanplecki@gmail.com>
2014-12-03 21:03:53 -0600

    Fix frontsite URLs

--- frontsite/urls.py
+++ frontsite/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 
 
@@ -7,10 +7,10 @@ urlpatterns = patterns('',
     url(r'^about/?$', TemplateView.as_view(template_name='aboutus.html'), name='about'),
     url(r'^login/?$', TemplateView.as_view(template_name='login.html'), name='login'),
 
-	url(r'^games', include([
-		url(r'^/?$', TemplateView.as_view(template_name='games.html'), name='games'),
-		url(r'^/weighin/?$', TemplateView.as_view(template_name='games/weighin.html'), name='weighin_game'),
-	]),
+    url(r'^games', include([
+        url(r'^/?$', TemplateView.as_view(template_name='games.html'), name='games'),
+        url(r'^/weighin/?$', TemplateView.as_view(template_name='games/weighin.html'), name='weighin_game'),
+    ])),
 
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 )
