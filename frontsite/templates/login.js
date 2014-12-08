42a46b892305c05d1b37015c7a266fa3c3ed5178
kazuuchikata <kazuuchikata@gmail.com>
2014-12-07 21:45:31 -0600

    fixed login function

--- frontsite/templates/login.js
+++ frontsite/templates/login.js
@@ -1,35 +1,17 @@
-// function login() {
-// 	alert("button pressed");
-// 	var user = $('#username').val();
-// 	var password = $('#pwd').val();
-
-// 	$.ajax({
-// 		type: "GET",
-// 		url: "http://dev.weighin.me/api/1/login/",
-// 		crossDomain: true,
-// 		data: {
-// 			username: user,
-// 			password: password
-// 		},
-// 		success: function() {
-// 			alert("success");
-// 		}
-// 	});
-// }
-$(document).ready(function() {
-            $("button").click(function(){
-            alert("button pressed");
-            $.getJSON("http://dev.weighin.me/api/1/login/?username=" + $('#username').val() + "&password=" + $('#pwd').val(),function(result)
-            {
-            if(result['success'] == "True")
-                {
-         			alert("Success bitch!");
-                }
-            else
-                {
-                    alert("Account not found. Try logging in again");
-                }
-
-         });
-      });
-});
+function login() {
+	alert("button pressed");
+		var user = 'Andrew';
+		var password = '1234567890';
+	$.ajax({
+		type: 'GET',
+		url: "http://dev.weighin.me/api/1/login/",
+		crossDomain: true,
+		data: {
+			username: user,
+			password: password
+		},
+		success: function() {
+			alert("success");
+		}
+	});
+}
