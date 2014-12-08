9e20adf2cf14884d48cc501b8d424c8693779e06
kazuuchikata <kazuuchikata@gmail.com>
2014-12-07 20:41:12 -0600

    testing out login function

---
+++ frontsite/templates/login.js
@@ -0,0 +1,35 @@
+// function login() {
+// 	alert("button pressed");
+// 	var user = $('#username').val();
+// 	var password = $('#pwd').val();
+
+// 	$.ajax({
+// 		type: "GET",
+// 		url: "http://dev.weighin.me/api/1/login/",
+// 		crossDomain: true,
+// 		data: {
+// 			username: user,
+// 			password: password
+// 		},
+// 		success: function() {
+// 			alert("success");
+// 		}
+// 	});
+// }
+$(document).ready(function() {
+            $("button").click(function(){
+            alert("button pressed");
+            $.getJSON("http://dev.weighin.me/api/1/login/?username=" + $('#username').val() + "&password=" + $('#pwd').val(),function(result)
+            {
+            if(result['success'] == "True")
+                {
+         			alert("Success bitch!");
+                }
+            else
+                {
+                    alert("Account not found. Try logging in again");
+                }
+
+         });
+      });
+});
