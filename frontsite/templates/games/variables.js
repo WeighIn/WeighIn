72ef27695201e27d2f1eb02bc27c7212ddd2a114
Radhir Kothuri <kothuri2@illinois.edu>
2014-12-09 14:14:42 -0600

    fix weighin game

--- frontsite/templates/games/variables.js
+++
@@ -1,7 +0,0 @@
-var linkArray = [
-	"https://s3.amazonaws.com/weighin/game0/task1.png",
-	"https://s3.amazonaws.com/weighin/game0/task2.png",
-	"https://s3.amazonaws.com/weighin/game0/task3.png",
-	"https://s3.amazonaws.com/weighin/game0/task4.png",
-	"https://s3.amazonaws.com/weighin/game0/task5.png"
-  ];
